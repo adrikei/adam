f59493fb70c127d59472681a4cf0d45435a49645
adrikei <adrikei@gmail.com>
2022-04-23 13:46:55 +0100

    feat: :sparkles: I am interactive

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("I am Adam.");
+const readline = require("readline");
+
+const prompt = (promptMessage, digest) => {
+  var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question(promptMessage, (input) => {
+    digest(input);
+    rl.close();
+  });
+};
+
+const interact = (respondTo) => (input) => {
+  console.log("You said:\n" + input);
+  if (input === "exit") {
+    console.log("kthanxbye");
+    process.exit(0);
+  }
+  const output = respondTo(input);
+  console.log("I say:\n" + output);
+};
+
+const fallback = () => "I am Adam.";
+
+prompt("> ", interact(fallback));
